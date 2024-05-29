2cbbd00465eeb5d8b7952efe077d66af3054e80a
Monika Badrukhiya <monikabadrukhiya@gmail.com>
2024-05-29 20:25:11 +0530

    Initial commit

---
+++ Python/index.py
@@ -0,0 +1,18 @@
+a=10
+print(type(a))
+
+a="wlecome"
+print(type(a))
+
+b=12.30
+print("type=",type(b))
+ 
+a=30
+b=40
+if a>b:
+    {
+        print("a is max")
+    }
+else:
+    print("b is max")
+
---
+++ Python/task1.py
@@ -0,0 +1,13 @@
+n=124
+rev=0
+rem=n%10
+
+
+if n%10:
+   rev=rev*10+rem
+   print("rev=",rev)
+elif:
+     n=n/10
+     print("n=",rem)  
+
+
