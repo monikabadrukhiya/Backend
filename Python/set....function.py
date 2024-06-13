526e965bb7d396c7cca488817ec39337bbe93a7d
Monika Badrukhiya <monikabadrukhiya@gmail.com>
2024-06-13 20:38:16 +0530

    done

--- Python/set....function.py
+++ Python/set....function.py
@@ -6,18 +6,18 @@
 # 1:add()= add item in element=====================
 # date={1,4,7,5,2,4}
 # date.add(10)
-# print(date)
+# print(date)                   #{1, 2, 4, 5, 7, 10}
 
 # 2:update()= add item from another set into the current set,join to set element==========
 # s={1,2,3}
 # s1={11,12,13}
 # s.update(s1)
-# print(s)
+# print(s)                      #{1, 2, 3, 11, 12, 13}
         # iterable list to set using update===========
 # s1={11,12,13}
 # s2=[100,200]
 # s1.update(s2)
-# print(s1)
+# print(s1)             #{100, 200, 11, 12, 13}
 
 # 3.remove(),discard()=to remove item in a set========================
 # s={'c','d','m','i'}
@@ -33,102 +33,107 @@
 # so you will not know what item that gets removed
 # s={12,11,1,4,10}
 # s.pop()
-# print(s)
+# print(s) 
 
 # 5.clear()= empties the set=============
 # s={3,4,5,6}
 # s.clear()
-# print(s)
+# print(s)              #set{}
 
 # 6.del()=delete the set comletely=========================
 # s={2,3,4,5}
 # del s
-# print(s)
+# print(s)              #eror
 
 # set operators//
 # pip operators(|)=used to remove dublicate items===================
 # d1={"mon","tues","sun","wed","sun"}
 # d2={"mon","thus","wed"}
-# print(d1|d2)
+# print(d1|d2)                        #{'wed', 'tues', 'mon', 'thus', 'sun'}   
 
 # 7.union()= that returns a new set containing all items from both sets==========
 # d1={"mon","tues","sun","wed","sun"}
 # d2={"mon","thus","wed"}
-# print(d1.union(d2))
+# print(d1.union(d2))                   #{'wed', 'tues', 'mon', 'thus', 'sun'}
 
 # 8.update()=that insert all the items from one set into another=============
 # d1={"mon","tues","sun","wed","sun"}
-# d2={1,2,3}
+# d2={"mon","tues",'thus'}
 # d1.update(d2)
-# print(d1)
+# print(d1)                          #{'tues', 'mon', 'sun', 'wed', 'thus'}
 
 # and & operators//
 # and operators(&)= smae items can be returns========
 # d1={"mon","tues","sun","wed","sun"}
 # d2={"mon","thus","wed"}
-# print(d1&d2)
+# print(d1&d2)                                 #{'mon', 'wed'}
 
 # 9.intesection()= it modified the original set by removing the unwanted itemd
 # this method return new set =============
 # d1={"mon","tues","sun","wed","sun"}
 # d2={"mon","thus","wed"}
-# print(d1.intersection(d2))
+# print(d1.intersection(d2))                         #{'mon', 'wed'}
 
 # 10.intersection_update()=remove the items from the original set that are not
 #  present in both the sets =======
 # d1={"mon","tues","sun","wed","sun"}
 # d2={"mon","thus","wed"}
 # d1.intersection_update(d2)
-# print(d1)
+# print(d1)                                         #{'mon', 'wed'}
 
 # subtraction opretor//
 # subtraction opretor(-)=orignal sets remove items presernt in onther set and 
 # returns new set
 # d={"moni","panvi","keyuri","kinjal"}
 # d1={"mohit","khushal","kinjal","moni"}
-# print(d-d1)
+# print(d-d1)                                  #{'panvi', 'keyuri'}
 
 # 11.difference()= in both set defference item can be return in original set =======
 # d={"moni","panvi","keyuri","kinjal"}
 # d1={"mohit","khushal","kinjal","moni"}
-# print(d.difference(d1))
+# print(d.difference(d1))                         # {'panvi', 'keyuri'}
 
 # 12.difference_update()===
 # d={"moni","panvi","keyuri","kinjal"}
 # d1={"mohit","khushal","kinjal","moni"}
-# d1.difference(d)
-# print(d1)
+# d.difference_update(d1)
+# print(d)                                          #{'panvi','keyuri'}
 
 
 # 13.symmentric_difference()=it remove that element which is present in both sets==========
 # s={"a","b","c"}
 # s1={"a",2,3}
 # s3=s.symmetric_difference(s1)
-# print(s3)
+# print(s3)                                  #{2, 3, 'b', 'c'}
 
 # 14.symmentric_difference_update()=will keep only the elements that are not 
 # present in both sets
 # s={"a","b","c"}
 # s1={"a",2,3}
 # s.symmetric_difference_update(s1)
-# print(s)
+# print(s)                                         #{'b', 3, 2, 'c'}
 
 # 15.copy()=returns a copy of the set========
 # s={1,2,3,4,5}
 # s.copy()
-# print(s)
+# print(s)                    #{1, 2, 3, 4, 5}
 
 #16.isdisjoint()=returns whether two sets have a intersection or not===========
 # s={1,2,3,4,5}
-# s1={1,2,3,4,5}
-# print(s.isdisjoint(s1))
+# s1={6,7,8}
+# print(s.isdisjoint(s1))       #true
+
+# ====false=====
+# s={1,2,3,4,5}
+# s1={1,2,3}
+# print(s.isdisjoint(s1))    #false
 
 # 17.issubset() =returns whether another set contains this set or not=====
 # s={1,2,3,4,5}
 # s1={11,12,13,14}
-# print(s.issubset(s1))
+# print(s.issubset(s1))   #false
 
 # 17.issubset() =returns whether this set contains another set or not=====
-s={1,2,3,4,5}
-s1={1,2,3,}
-print(s.issuperset(s1))
+# s={1,2,3,4,5}
+# s1={1,2,3,}
+# print(s.issuperset(s1))          #true
--- Python/string.....function.py
+++ Python/string.....function.py
@@ -4,59 +4,85 @@
 
 # slice==============================
 # str="hello monika"
-# print(str[0:])
-# print(str[0:5])
-# print(str[3:8])
-# print(str[-1])
-# print(str[-3])
-# print(str[-7:-3])
+# print(str[0:])                                  #hello monika
+# print(str[0:5])                                  #hello
+# print(str[3:8])                                  #lo mo
+# print(str[-1])                                   #a
+# print(str[-3:-1])                                #ik
+# print(str[-7:-3])                                #mon
 
 # # print reverse string
-# print(str[::-1])     
+# print(str[::-1])                                  #akinom olleh
 
-# 1.capitalize()=it capitalize the first character of the string=============
+# //////////////////////////////////////////////////////////////////////////////
+# 1.capitalize()=it capitalize the first character of the string
 # str="monika"
-# print(str.capitalize())
+# print(str.capitalize())                       #Monika
 
-# 2.casefold()===it return a string to lowercase in such case===========
+# //////////////////////////////////////////////////////////////////////////////
+# 2.casefold()===it return a string to lowercase in such case
 # str="Creaive Institute"
-# print(str.casefold())
+# print(str.casefold())                         #creative Institute
 
-# 3.count()==count char in a string between begin and end index===
+# ///////////////////////////////////////////////////////////////////////////////
+# 3.count()==count char in a string between begin and end index
 # str="Creative Institute"
-# str2=str.count('a',2,7)
-# print(str2)
+# str2=str.count('e')                  
+# print(str2)                                   #2
 
-# 4.Endswith()=string terminate with specified char or not returns ture/false========
 # str="Creative Institute"
+# str2=str.count('e',1,10)
+# print(str2)                                   #3
+
+# ////////////////////////////////////////////////////////////////////////////////////
+# 4.Endswith()=string terminate with specified char or not returns ture/false
+# str="Creative Institute."
 # str2=str.endswith('.')
-# print(str2)
+# print(str2)                                    #false
 
-# 5.find()==it return string between start and end index==================
+# str="Creative Institute"
+# str2=str.endswith('a',1,4)
+# print(str2)                 #last pos thi ek pos agal count karine true/false ans ape    
+
+# /////////////////////////////////////////////////////////////////////////////
+# 5.find()==it return string between start and end index
 # str="Creative Institute "
 # str2=str.find("Creative")
-# print(str2)
+# print(str2)                   #0/true
+                                #-1/false
 
-# 6.format()==it returns formatted version of s,using the passed value============
+# ///////////////////////////////////////////////////////////////////////////////////
+# 6.format()==it returns formatted version of set,using the passed value
 # str="java "
 # str2="python"
-# print("{1} and {2} are programin language".format(str,str2))
+# print("{} and {} are programing language".format(str,str2)) 
+                            #java  and python are programin language
 
-# 7.index()=
+# str="c# "
+# str2="Html"
+# print("{0} and {1} are programing language".format(str,str2)) 
+                            #c#  and Html are programing language
+
+
+# ////////////////////////////////////////////////////////////////////////////
+# 7.index()= pass string first index can be return
 # str="java & pyhton are programing language"
 # str1=str.index("ming")
-# print(str1)
+# print(str1)                              #24
 
-# 8.isalnum=find given string is alphanumeric or not====
-# alphanumeric=character which is a letter or number is know as alphanumeric======
+
+# //////////////////////////////////////////////////////////////////////////////
+# 8.isalnum=find given string is alphanumeric or not
+# alphanumeric=character which is a letter or number is know as alphanumeric
 # str="creative123"
 # str2=str.isalnum()
 # print(str2)
 
-# 9.isalpha =check string character are alphabetic or not return true/false======
-# str="creative"
-# str2=str.isalpha()
-# print(str2)
+# ///////////////////////////////////////////////////////////////////////////////
+# 9.isalpha =check string character are alphabetic or not return true/false
+str="creative"
+str2=str.isalpha()
+print(str2)
 
 # 10.isdecimal =check all the charcer in string are decimal or not return true/false====
 # str="123"
